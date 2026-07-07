3e63c5162a33d8232af89e4346cf74a32ff3fb15
Sudha Madhuri Gathala <156404465+SudhaMadhuri13@users.noreply.github.com>
2026-07-07 19:29:54 +0530

    Add files via upload

---
+++ LLM_LCEL/serve.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_groq import ChatGroq
+from langserve import add_routes
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+groq_api_key=os.getenv("GROQ_API_KEY")
+model=ChatGroq(model="llama-3.3-70b-versatile",groq_api_key=groq_api_key)
+
+##Prompt templates
+
+generic_template= "Transulate the following into {language}:"
+prompts=ChatPromptTemplate.from_messages( [("system",generic_template),("user","{text}")])
+
+parser=StrOutputParser()
+
+#chain 
+chain= prompts|model|parser
+
+app=FastAPI(title="Langchain Server",version="1.0", description= "A simple API server using Langchain runnable interface ")
+#adding chain routes 
+add_routes(
+    app,chain,path="/chain"
+)
+   
+if __name__=="__main__":
+    import uvicorn
+    uvicorn.run(app,host="localhost",port=8000)
